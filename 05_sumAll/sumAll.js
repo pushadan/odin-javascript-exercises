26599f95e465de445475d14ea2dda41d51cde508
pushadan <35050497+pushadan@users.noreply.github.com>
2023-12-29 05:00:57 -0800

    Finish 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(startNum,endNum) {
+    let numbers = [startNum];
+    let sum = startNum;
+    let index = 1;
 
+    if (typeof startNum != "number" || 
+        typeof endNum != "number" || 
+        Math.sign(startNum) === -1 || 
+        Math.sign(endNum) === -1)
+        {return "ERROR";}
+    else if (startNum < endNum){
+        for (let j = startNum+1; j <= endNum; j++){
+            numbers.push(j)
+            sum += numbers[index];
+            index++;
+        }
+    }
+    else{
+        for (let j = startNum-1; j >= endNum; j--){
+            numbers.push(j)
+            sum += numbers[index];
+            index++;
+        }
+    };
+    return sum;
 };
 
 // Do not edit below this line
