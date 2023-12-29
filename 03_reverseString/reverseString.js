848ce9f98c3f061147e58277ff38e08c183b2c55
pushadan <35050497+pushadan@users.noreply.github.com>
2023-12-28 22:03:39 -0800

    Finish 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splitStringArray = string.split("");
+    splitStringArray = splitStringArray.reverse();
 
+    let reversed = "";
+    for (const letter of splitStringArray){
+        reversed += letter;
+    };
+
+    return reversed;
 };
 
 // Do not edit below this line
