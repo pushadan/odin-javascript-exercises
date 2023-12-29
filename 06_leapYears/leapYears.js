12277dde20a4005481f5ec0a24b1937b7618825c
pushadan <35050497+pushadan@users.noreply.github.com>
2023-12-29 05:28:52 -0800

    Finish 06_leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //leap year rules:
+    //  - divisible by 4
+    //  - not divisible by 100, divisible by 400 
+    //  - not divisible by 100
+    if (year % 100 === 0 && year % 400 !== 0){
+        return false;
+    }
+    else if (year % 4 !== 0){
+        return false;
+    }
 
-};
+    return true;
+};  
 
 // Do not edit below this line
 module.exports = leapYears;
