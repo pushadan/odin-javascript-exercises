66c2b460d4fb44131db58bf5ff132495607093fb
pushadan <35050497+pushadan@users.noreply.github.com>
2023-12-29 03:27:38 -0800

    Finish 04removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
+    let list = arguments[0];
 
+    argumentsLength = (arguments.length)-1;
+    for (let i = 1; i <= argumentsLength; i++){
+        index = list.indexOf(arguments[i]);
+        if (index === -1){
+            continue;
+        }
+        list.splice(index, 1);
+    }
+
+    return list
 };
 
 // Do not edit below this line
