bd26e425cd5c65126066bbb768e9bfcac87f8608
pushadan <35050497+pushadan@users.noreply.github.com>
2023-12-28 20:29:32 -0800

    Finish 01_helloWorld and 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    
+    let string = "";
+    for (let i=0;i<num;i++){
+        string += word;
+    };
+    if (num === 0){return string}
+    else if (num < 0){return "ERROR"};
+    return string;
 };
 
 // Do not edit below this line
